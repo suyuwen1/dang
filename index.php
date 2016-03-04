16ca71cb5075faec659a37040e7df6fe1980a6d9
suyuwen1 <suyuwen1@126.com>
2016-03-04 14:17:23 +0800

    2016.3.4

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
 <title>Examples</title>
 <meta name="description" content="">
 <meta name="keywords" content="">
 <link href="css/style.css" rel="stylesheet">
 <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="js/SuperSlide/jquery.SuperSlide.2.1.1.js"></script>
 <script type="text/javascript" src="js/main.js"></script>
 </head>
 <body>
@@ -24,20 +26,36 @@
     		<div class="bars-list"><a target="_blank" href="#">联系方式</a></div>
     	</div>
     </div>
-    <div id="banner">
-    	<img src="img/1.jpg">
-    </div>
+    <div id="banner" class="slideBox">
+        <div class="hd">
+            <ul><li>1</li><li>2</li><li>3</li><li>4</li></ul>
+        </div>
+        <div class="bd">
+            <ul>
+                <li><a href="#" target="_blank"><img src="img/1.jpg" /></a></li>
+                <li><a href="#" target="_blank"><img src="img/1.jpg" /></a></li>
+                <li><a href="#" target="_blank"><img src="img/1.jpg" /></a></li>
+                <li><a href="#" target="_blank"><img src="img/1.jpg" /></a></li>
+			</ul>
+		</div>
+
+		<!-- 下面是前/后按钮代码，如果不需要删除即可 -->
+		<a class="prev" href="javascript:void(0)"></a>
+		<a class="next" href="javascript:void(0)"></a>
+        
+		</div>
+    <script type="text/javascript">jQuery(".slideBox").slide({mainCell:".bd ul",effect:"left",autoPlay:true});</script>
     <div id="main">
-    	<div id="myjs" class="title"><h2>名医介绍</h2><span>010-12345678</span></div>
-    	<div class="con" style="height:213px">
+    	<div id="myjs" class="title"><h2>名医介绍</h2><a class="title_tel" href="tel:010-12345678">010-12345678</a></div>
+    	<div class="con">
 			<div class="con-myjs-left"><div class="con-myjs-left-img"><img src="img/titleleft.png"></div></div>
 			<div class="con-myjs-rigth">
 				<div class="con-myjs-rigth-top"><img src="img/dangdaifu.png"></div>
 				<div class="con-myjs-rigth-con">fasghfklasdfhjsaldsafghkskkadslfhakfjhasdgk'jasghkjashfkjasdf3245j1k325h43jkhdfjahfksdaf是朝秦暮楚蝇aksd革查村果sdjk工虽土木工程</div>
 			</div>
     	</div>
-    	<div id="bqfl" class="title"><h2>病情分类</h2><a class="mfzx">免费咨询</a></div>
-    	<div class="con" style="height:633px">
+    	<div id="bqfl" class="title"><h2>病情分类</h2><a target="_blank" href="http://p.qiao.baidu.com//im/index?siteid=8240151&ucid=19386485" class="mfzx">免费咨询</a></div>
+    	<div class="con">
 			<div class="con-bqfl-top">
 				<span class="con-bqfl-top-list">常见皮肤病：</span>
 				<a href="#">青春痘</a>
@@ -62,11 +80,51 @@
 				</div>
 			</div>
     	</div>
-    	<div id="cgal" class="title"><h2>成功案例</h2><span>010-12345678</span></div>
-    	<div class="con cgal"><div class="cgal-w"><div class="cgal-con"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"><img src="img/titleleft.png"></div></div></div>
-    	<div id="yfzs" class="title"><h2>预防知识</h2><a class="mfzx">免费咨询</a></div>
+    	<div id="cgal" class="title"><h2>成功案例</h2><a class="title_tel" href="tel:010-12345678">010-12345678</a></div>
+    	<div class="con cgal">
+            <div class="picMarquee-left">
+			<div class="bd">
+				<ul class="picList">
+					<li>
+						<div class="pic"><a href="#" target="_blank"><img src="img/titleleft.png" /></a></div>
+					</li>
+					<li>
+						<div class="pic"><a href="#" target="_blank"><img src="img/titleleft.png" /></a></div>
+					</li>
+					<li>
+						<div class="pic"><a href="#" target="_blank"><img src="img/titleleft.png" /></a></div>
+					</li>
+					<li>
+						<div class="pic"><a href="#" target="_blank"><img src="img/titleleft.png" /></a></div>
+					</li>
+					<li>
+						<div class="pic"><a href="#" target="_blank"><img src="img/titleleft.png" /></a></div>
+					</li>
+					<li>
+						<div class="pic"><a href="#" target="_blank"><img src="img/titleleft.png" /></a></div>
+					</li>
+					<li>
+						<div class="pic"><a href="#" target="_blank"><img src="img/titleleft.png" /></a></div>
+					</li>
+					<li>
+						<div class="pic"><a href="#" target="_blank"><img src="img/titleleft.png" /></a></div>
+					</li>
+					<li>
+						<div class="pic"><a href="#" target="_blank"><img src="img/titleleft.png" /></a></div>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<script type="text/javascript">
+		jQuery(".picMarquee-left").slide({mainCell:".bd ul",autoPlay:true,effect:"leftMarquee",vis:5,interTime:50});
+		</script>
+        </div>
+    	<div id="yfzs" class="title"><h2>预防知识</h2><a target="_blank" href="http://p.qiao.baidu.com//im/index?siteid=8240151&ucid=19386485" class="mfzx">免费咨询</a></div>
     	<div class="con">sadf</div>
     </div>
+    <div id="leftgn"></div>
     <div id="footer"></div>
+    <script type="text/javascript"> var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://"); document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fdd4c9118206ce15d0735e003bf5c9daa' type='text/javascript'%3E%3C/script%3E")) </script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 $(function(){
 	bars_xf();
-	cgal_scroll();
-	cgal_scroll_s();
-	cgal_scroll_hover();
 })
 
 function bars_xf(){//导航栏悬浮
@@ -15,19 +12,32 @@ function bars_xf(){//导航栏悬浮
 	$(".bars-list-a").click(function(event) {
 		var name=$(this).attr('name');
 		var v=$("#"+name).offset().top;
-		$(window).scrollTop((v-72))
+		// $(window).scrollTop((v-72))
+        var explorer =navigator.userAgent;
+        if(!explorer.match(/Mobile/i)){
+            $('html,body').animate({scrollTop:(v-72)},'fast');
+        }else{
+            $('html,body').animate({scrollTop:(v)},'fast');
+        }
 	});
 	$(window,document).scroll(function(event) {
 		var s=$(this).scrollTop();
-		if (s>b) {
-			$("#bars").addClass('bars-xf');
-			$("#banner").css('margin-top', '53px');
-		} else{
-			$("#bars").removeClass('bars-xf');
-			$("#banner").removeAttr('style');
-			// 
-		}
-		// console.log('s='+s+', '+'b='+b+', m='+m+', l='+l+', c='+c+', y='+y);
+        
+        //console.log('s='+s+', '+'b='+b+', m='+m+', l='+l+', c='+c+', y='+y);
+        // console.log($("#myjs").scrollTop());
+        var explorer =navigator.userAgent;
+        if(!explorer.match(/Mobile/i)){
+            if (s>b) {
+                $("#bars").addClass('bars-xf');
+                $("#banner").css('margin-top', '53px');
+            } else{
+                $("#bars").removeClass('bars-xf');
+                $("#banner").removeAttr('style');
+            }
+        }
+		
+        
+		// // console.log('s='+s+', '+'b='+b+', m='+m+', l='+l+', c='+c+', y='+y);
 		if (s<(m-73)||s>=f) {
 			$(".bars-list a").removeClass('bars-list-a1');
 		}
@@ -47,31 +57,5 @@ function bars_xf(){//导航栏悬浮
 			$(".bars-list a").removeClass('bars-list-a1');
 			$(".bars-list a[name='yfzs']").addClass('bars-list-a1');
 		}
-	});
-}
-var s_t='';
-function cgal_scroll_s(){
-	cgal_scroll_e();
-	s_t=setInterval("cgal_scroll()",20);
-}
-function cgal_scroll_e(){
-	clearInterval(s_t);
-	s_t='';
-}
-function cgal_scroll(){
-	var l=$(".cgal-con").position().left;
-	var a=0;
-	if (Math.abs(l)>=966) {
-		a='0px';
-	} else{
-		a=(l-1)+'px';
-	};
-	$(".cgal-con").css('left', a);
-}
-function cgal_scroll_hover(){
-	$(".cgal-w").hover(function() {
-		cgal_scroll_e();
-	}, function() {
-		cgal_scroll_s();
 	});
 }
